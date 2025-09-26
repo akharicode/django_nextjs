7aa23579ee280078ca4292ceed2a49474f3f334b
informatica-aconcagua <informaticas.aconcagua@gmail.com>
2025-09-26 00:05:18 -0300

    eliminacion view.py

--- core/urls.py
+++ core/urls.py
@@ -19,10 +19,8 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import TestView
 
 urlpatterns = [
-    path('test/', TestView.as_view(), name='test'),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
